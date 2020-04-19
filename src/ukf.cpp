a99cfa903f5272c388ce3e09f62713f91abe6d15
sleeping-bag <mail.satyajit.b@gmail.com>
2020-04-19 12:53:26 +0200

    Update longitudinal acceleration noise param

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -22,7 +22,7 @@ UKF::UKF()
   P_ = MatrixXd(5, 5);
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 6;
+  std_a_ = 2;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
   std_yawdd_ = 1;
